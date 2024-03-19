a2969872db358c447de30bc932e3cea83bcb5926
sumitgupta214 <Sumitgupta993577@gmail.com>
2024-03-19 20:05:18 +0530

    Some required
    changes

--- gallery.php
+++ gallery.php
@@ -20,7 +20,7 @@
             <div id="sports" class="image-block">
                 <h1> SPORTS </h1>
                 <div class="img-area">
-                    <div class="single-img"><img src="./Assets/Facilities/S4.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
+                    <div class="single-img"><img src="./Assets/Facilities/S4.jpg" width="360" height="310" id="myImg" style="border-radius: 10px;" alt=""></div>
                     <div class="single-img"><img src="./Assets/Facilities/S5.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
                     <div class="single-img"><img src="./Assets/Facilities/S6.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
                     <div class="single-img"><img src="./Assets/Gallery/S6.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
@@ -37,9 +37,21 @@
                     <div class="single-img"><img src="./Assets/Gallery/S17.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
                     <div class="single-img"><img src="./Assets/Gallery/S18.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
                     <div class="single-img"><img src="./Assets/Gallery/S19.jpg" width="360" height="310" style="border-radius: 10px;" alt=""></div>
-
                 </div>
+
             </div>
+
+        </div>
+        <div id="myModal" class="modal">
+
+            <!-- The Close Button -->
+            <span class="close">&times;</span>
+
+            <!-- Modal Content (The Image) -->
+            <img class="modal-content" id="img01">
+
+            <!-- Modal Caption (Image Text) -->
+            <div id="caption"></div>
         </div>
     </section>
 
@@ -69,6 +81,28 @@
 
     </section>
     <?php include 'footer.html' ?>
+
+
+
+    <script>
+        var modal = document.getElementById("myModal");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = document.getElementById("myImg");
+        var modalImg = document.getElementById("img01");
+        img.onclick = function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        }
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    </script>
 </body>
 
 </html>
